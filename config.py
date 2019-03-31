a0d9354b3164d9ffdd6a7327bcd56d7ca0c5f423
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-03-31 15:00:59 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+token = 'NTYxODM0NjExMTU2NTgyNDIw.XKCDxQ.Xao2D3p-ufDZUBMTYlrPMy7rq4A' #Put Your bots token here
+
+prefix = '!'
+
+link = '' #put bot invite link here
+
+ownerid = '358661627459141642' #put your id here
+
